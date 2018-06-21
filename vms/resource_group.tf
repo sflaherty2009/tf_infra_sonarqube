23a7859115ba8d427b7c504b497092537f617ee8
Scott Flaherty <scott_flaherty@trekbikes.com>
2018-06-21 12:55:39 -0500

    addition of resource group lock

--- vms/resource_group.tf
+++ vms/resource_group.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg" {
   name     = "${var.resource_group_name}-${terraform.workspace}"
   location = "${var.location}"
 }
+
+resource "azurerm_management_lock" "sonarqube_lock" {
+  name       = "DoNotDelete"
+  scope      = "${azurerm_resource_group.rg.id}"
+  lock_level = "CanNotDelete"
+  notes      = "Implemented as part of DVO-3231"
+}
